41905262af7ba9122815e81cf69761cfa79c75ef
aibalit <aibar.assangali@gmail.ru>
2019-02-08 17:31:45 +0600

    asd

---
+++ week1/Task1/Prime_numbers/Prime_numbers/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine()); // read integer n
+
+			int[] mas = new int[n]; // create array mas
+			int[] mas2 = new int[n]; // create array mas2   
+
+			string[] nums = Console.ReadLine().Split(new char[] { ',', ';', '#', ' ' }); //read numbers from input to array of string
+
+			for (int i = 0; i < n; i++)
+			{
+				mas[i] = int.Parse(nums[i]); // convert from string to our array of integers
+			}
+
+			int k = 0; //  adding counter
+			for (int i = 0; i < n; i++) 
+			{
+				for (int j = 1; j <= mas[i]; j++) // cycle of dividers
+				{
+					if (mas[i] % j == 0) // condition
+					{
+						k++; // adjunction for counter
+					}
+				}
+				if (k == 2) // condition
+				{
+					mas2[i] = mas[i]; // result if condition is correct
+				}
+				else
+					mas2[i] = 0; // another result 
+				k = 0; // counter resfresh
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				if (mas2[i] != 0) // condition
+					k++; // adjunction for counter
+			}
+
+			Console.WriteLine(k); // shows the counter
+
+			for (int i = 0; i < n; i++)
+			{
+				if (mas2[i] != 0)
+					Console.Write($"{mas2[i]} "); // this command allows you to use reserved word with result of problem
+			}
+
+			Console.ReadKey();
+
+		}
+	}
+}
---
+++ week1/Task3/repeated_value/repeated_value/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());// adding the method
+
+			Method(n);
+
+		}
+
+		static void Method(int n)// main part 
+		{
+			string[] s = Console.ReadLine().Split(' '); //read numbers from input to array of string
+
+			int[] arr = new int[n * 2];// creating second array with 2*n values
+
+			int j = 0;//index of value
+			for (int i = 0; i < s.Length; i++)
+			{
+				arr[i + j] = int.Parse(s[i]);// adding the value to array
+				arr[i + j + 1] = int.Parse(s[i]);// adding copied vallue to array to the next cell
+				j++;
+			}
+
+			foreach (int i in arr)// output the values of array
+			{
+				Console.Write($"{i} "); //this command allows you to use reserved word with result of problem
+			}
+		}
+	}
+}
